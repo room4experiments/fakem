dc5fe57a9893428ee5df928a6097924857c9d696
dimitrijevic <dimitrijevic@gmail.com>
2023-02-06 11:10:15 +0000

    Initial project commit

---
+++ fakem/Controllers/MatlabCallController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System;
+using MathWorks.MATLAB.NET.Arrays;
+
+namespace fakem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MatlabCallController : ControllerBase
+    {
+        private readonly ILogger<MatlabCallController> _logger;
+
+        public MatlabCallController(ILogger<MatlabCallController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Matlab")]
+        public async Task<IActionResult> Get()
+        {
+
+            using (fake_json.fake1 obj = new fake_json.fake1())
+            {
+                MWArray inputString = new MWCharArray("[]");
+                MWArray res = obj.fake_json(inputString);
+                return Ok(res.ToString());
+            }
+        }
+    }
+}
